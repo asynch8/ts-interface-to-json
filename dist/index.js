e25088087a0b3526796b129dc7ced3861b05eda7
Asynch8 <Asynch8@gmail.com>
2024-05-09 15:04:17 +0200

    Updated module to work again

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,9 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const ts_interface_1 = require("./ts-interface");
+const ts_interface_1 = __importDefault(require("./ts-interface"));
 function convert(typeName, path) {
     const types = (0, ts_interface_1.default)(typeName, path);
     // console.log({ types })
---
+++ types.d.ts
@@ -0,0 +1,3 @@
+declare module 'ts-interface-to-json-schema' {
+    export default function convert(interfaceName: string, definitionFile: string): any;
+}
